dde54a27e4a1f1af67433c797ffdfc7b666a7b98
lea.sassmannshausen <lea.sassmannshausen@hotmail.de>
2021-10-08 13:23:44 +0200

    cmakeTest

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -2,8 +2,8 @@ INCLUDE(TribitsListHelpers)
 
 SET( FEDDlib_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
   core			feddlib/core        PT
-    amr				feddlib/amr    PT
-    problems		feddlib/problems    PT
+  problems		feddlib/problems    PT
+  amr				feddlib/amr    PT
 
 
 
