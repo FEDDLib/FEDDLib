77a75a060428997e409fd768ca24ff0bfbff9acc
Lea <lea.sassmannshausen@hotmail.de>
2023-05-07 12:44:30 +0200

    Test with newest Trilinos Version 6th Mai

--- feddlib/core/Mesh/MeshPartitioner_decl.hpp
+++ feddlib/core/Mesh/MeshPartitioner_decl.hpp
@@ -46,6 +46,7 @@ namespace FEDD {
     In general elements follow a hierarchical structure. For example a 3D tetrahedral element owns all 2D and 1D entities (triangle/surface, edges) that are part of the surface. 
 
     As of now each surface and element node list is sorted to improve find/search operations. This influences for example the calculation of surface normals.
+    
     */
 template <class SC = default_sc, class LO = default_lo, class GO = default_go, class NO = default_no>
 class MeshPartitioner {
