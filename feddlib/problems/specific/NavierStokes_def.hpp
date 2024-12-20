a2bb9585d68d84e19553577df8f89463d74b632d
Lea <lea.sassmannshausen@hotmail.de>
2024-12-20 13:26:55 +0100

    Specifiying scaling of Mp in PCD for BDF 2

--- feddlib/problems/specific/NavierStokes_def.hpp
+++ feddlib/problems/specific/NavierStokes_def.hpp
@@ -578,7 +578,7 @@ void NavierStokes<SC,LO,GO,NO>::reAssemble(std::string type) const {
                 if(this->parameterList_->sublist("Timestepping Parameter").get("BDF",1) < 2) // BDF 1
                     Mp2->scale(1./dt);
                 else // BDF 2
-                    Mp2->scale(3./(2*dt));
+                    Mp2->scale(3./(2.*dt));
                 Mp2->fillComplete();
 
                 bcBlockMatrix->addBlock(Mp2,0,0);
