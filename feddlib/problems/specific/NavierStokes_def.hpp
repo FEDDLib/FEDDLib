4bfb3f93226850c5748c970ff6b92e78eaa95a39
Lea <lea.sassmannshausen@hotmail.de>
2025-02-23 20:05:14 +0100

    Add inititalize Prec again to monoltihic

--- feddlib/problems/specific/NavierStokes_def.hpp
+++ feddlib/problems/specific/NavierStokes_def.hpp
@@ -880,7 +880,7 @@ Teuchos::RCP<Thyra::PreconditionerBase<SC> > NavierStokes<SC,LO,GO,NO>::create_W
         stokesTekoPrecUsed_ = false;
     }
     else{
-        this->setupPreconditioner( type ); // initializePreconditioner( type );
+        initializePreconditioner( type ); // this->setupPreconditioner( type ); //
     }
     
 
