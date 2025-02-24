b3bbb6e633015ae88f7e04f92b9c305c54879942
Lea <lea.sassmannshausen@hotmail.de>
2025-02-24 17:02:09 +0100

    Flag process modified for unstructured good grid

--- feddlib/problems/examples/steadyNavierStokes/main.cpp
+++ feddlib/problems/examples/steadyNavierStokes/main.cpp
@@ -367,7 +367,7 @@ int main(int argc, char *argv[]) {
                 }
                 std::vector<double> parameter_vec(1, parameterListProblem->sublist("Parameter").get("MaxVelocity",1.));
 
-                domainVelocity->exportNodeFlags();
+                //domainVelocity->exportNodeFlags();
                 // domainVelocity->exportProcessor();
                 // ####################
                 Teuchos::RCP<BCBuilder<SC,LO,GO,NO> > bcFactory( new BCBuilder<SC,LO,GO,NO>( ) );
