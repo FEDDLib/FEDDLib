16e93022ab6dbdebf5a3f07d04da2f9fca370f91
Lea <lea.sassmannshausen@hotmail.de>
2025-07-17 11:13:32 +0200

    Using absolute value of flowrate

--- feddlib/core/General/BCBuilder_def.hpp
+++ feddlib/core/General/BCBuilder_def.hpp
@@ -592,7 +592,7 @@ void BCBuilder<SC,LO,GO,NO>::determineVelocityForFlowrate(LO i, double time) con
     // Then we have flowRateParabolic * u_max == Q  <=> u_max = Q/flowRateParabolic, and Q is given as 'desired flowrate' in flowrate
     double maxVelocity = flowRate[0] / std::fabs(flowRateParabolic);
     if(domain->getComm()->getRank() == 0)
-       cout << " --- Desired Flow Rate: " << flowRate[0] << " --- flowrate for parabolic inflow profile: " << flowRateParabolic  << " --- v_max based on flowrate and inflow profile: " << maxVelocity << " --- " << endl;
+       std::cout << " --- Desired Flow Rate: " << flowRate[0] << " --- flowrate for parabolic inflow profile: " << flowRateParabolic  << " --- v_max based on flowrate and inflow profile: " << maxVelocity << " --- " << std::endl;
     // Then we replace the parameter which contains the maximum velocity with the updated one
     vecBC_Parameters_[i][0] = maxVelocity;
 
