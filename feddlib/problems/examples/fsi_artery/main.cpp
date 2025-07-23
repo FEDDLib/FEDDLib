5c3daf9c8d9d3accfee65782ab1aa897ae9787f9
Lea <lea.sassmannshausen@hotmail.de>
2025-07-23 20:14:21 +0200

    Adding some parameters and updating flow direction

--- feddlib/problems/examples/fsi_artery/main.cpp
+++ feddlib/problems/examples/fsi_artery/main.cpp
@@ -144,7 +144,7 @@ void parabolicInflow3D(double* x, double* res, double t, const double* parameter
 
     res[0] = 0.;
     res[1] = 0.;
-    res[2] = -parameters[0]  * x[0];
+    res[2] = parameters[0]  * x[0];
 
 
     return;
