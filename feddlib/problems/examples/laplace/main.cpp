d9cae0441a27609e9ebc89554df0c05bf622d97b
Lea <lea.sassmannshausen@hotmail.de>
2025-07-25 13:20:17 +0200

    Update Laplace Solution

--- feddlib/problems/examples/laplace/main.cpp
+++ feddlib/problems/examples/laplace/main.cpp
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]) {
             ParameterListPtr_Type pListPartitioner = sublist( parameterListAll, "Mesh Partitioner" );
             MeshPartitioner<SC,LO,GO,NO> partitionerP1 ( domainP1Array, pListPartitioner, "P1", dim );
             
-            partitionerP1.readAndPartition();
+            partitionerP1.readAndPartition(15);
 
             if (FEType=="P2") {
                 domainP2.reset( new Domain<SC,LO,GO,NO>( comm, dim ));
@@ -170,7 +170,7 @@ int main(int argc, char *argv[]) {
                 domain = domainP1;
         }
 
-
+        domain->exportNodeFlags();
 
         // ####################
         Teuchos::RCP<BCBuilder<SC,LO,GO,NO> > bcFactory(new BCBuilder<SC,LO,GO,NO>( ));
