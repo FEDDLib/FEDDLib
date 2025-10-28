7f08f1040a531ade072c123079575554aade23ae
Lea <lea.sassmannshausen@hotmail.de>
2025-10-28 14:47:07 +0100

    Include hdf5 parallel header

--- feddlib/core/General/HDF5Toolbox.hpp
+++ feddlib/core/General/HDF5Toolbox.hpp
@@ -1,6 +1,10 @@
 #ifndef HDF5TOOLBOX_hpp
 #define HDF5TOOLBOX_hpp
 
+#include "hdf5.h"
+#ifdef HAVE_MPI
+#  include "H5FDmpio.h"
+#endif
 #include "HDF5Toolbox_decl.hpp"
 // #ifndef HAVE_EXPLICIT_INSTANTIATION
 // #include "HDF5_Export_def.hpp"
