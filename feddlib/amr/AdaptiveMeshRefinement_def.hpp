fefdce5ae924a50b4d245f505f90df7dcbf08ed1
Lea <lea.sassmannshausen@hotmail.de>
2025-10-28 16:05:30 +0100

    Remove close exporter from amr

--- feddlib/amr/AdaptiveMeshRefinement_def.hpp
+++ feddlib/amr/AdaptiveMeshRefinement_def.hpp
@@ -227,12 +227,7 @@ domainsP1_(0)
 }
 template <class SC, class LO, class GO, class NO>
 AdaptiveMeshRefinement<SC,LO,GO,NO>::~AdaptiveMeshRefinement(){
-	if(exporterSol_->isOpen())
-		exporterSol_->closeExporter();
-	if(exporterSol_->isOpen())
-		exporterSolP_->closeExporter();
-	if(exporterSol_->isOpen())
-		exporterError_->closeExporter();
+	
 }
 /*!
 \brief Initializing problem if only a certain area should be refined. 
