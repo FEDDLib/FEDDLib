26dae03dbeefe0bf8fedbdd5a8000626385bc1c6
Lea <lea.sassmannshausen@hotmail.de>
2025-10-28 14:58:41 +0100

    Add HDF Find TPL

--- feddlib/core/General/HDF5Toolbox.hpp
+++ feddlib/core/General/HDF5Toolbox.hpp
@@ -1,10 +1,7 @@
 #ifndef HDF5TOOLBOX_hpp
 #define HDF5TOOLBOX_hpp
 
-#include "hdf5.h"
-#ifdef HAVE_MPI
-#  include "H5FDmpio.h"
-#endif
+
 #include "HDF5Toolbox_decl.hpp"
 // #ifndef HAVE_EXPLICIT_INSTANTIATION
 // #include "HDF5_Export_def.hpp"
