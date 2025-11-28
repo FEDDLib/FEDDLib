10a8d0dfcc4f3f47c8cab893488e2c99f5524e41
Kyrill Ho <49488037+kyrillh@users.noreply.github.com>
2025-11-28 17:43:29 +0100

    Fix for missing MultiVector instantiation required by amr. (#120)

--- feddlib/core/LinearAlgebra/MultiVector.cpp
+++ feddlib/core/LinearAlgebra/MultiVector.cpp
@@ -6,6 +6,10 @@
 namespace FEDD {
     template class MultiVector<default_sc, default_lo, default_go, default_no>;
     template class MultiVector<default_lo, default_lo, default_go, default_no>;
+#ifdef DEFAULT_GO_IS_LONG_LONG
+    // TODO: this instantiation of MultiVector is used in amr. Is this necessary or would MultiVector<LO, LO, GO, NO> suffice?                                                                                                                           
+    template class MultiVector<default_go, default_lo, default_go, default_no>;
+#endif
 }
 #endif  // HAVE_EXPLICIT_INSTANTIATION
 #endif
